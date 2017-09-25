2d8220971558d817b9d3c1a03772bb293429a741
Bipin Manandhar <kubipin07@gmail.com>
2017-09-25 06:59:15 +0545

    Add simple form and use it with bootstrap for form creating

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
 
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
