374ccb1b499f905d0bc1dda31bc8bb72ec33f309
Bipin Manandhar <kubipin07@gmail.com>
2017-09-25 10:41:26 +0545

    Add is_owner? method for checking the owner of the comment when deleting it

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: :create
+  before_action :is_owner?, only: :destroy
 
   def create
     @post = Post.find(params[:post_id])
@@ -12,8 +13,9 @@ class CommentsController < ApplicationController
     end
   end
 
-  def index
-    @posts = Post.all
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to root_path
   end
 end
 
@@ -21,4 +23,11 @@ private
 
 def comment_params
   params.require(:comment).permit(:text, :post_id)
+end
+
+def is_owner?
+  @comment = Comment.find(params[:id])
+  if @comment.user != current_user
+    redirect_to root_path
+  end
 end
